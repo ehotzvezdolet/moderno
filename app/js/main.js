611f351b94f8974966f1d3d5520172a4ce020529
ehotzvezdolet <ehotzvezdolet@gmail.com>
2021-01-02 14:31:32 +0300

    Sliders

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function(){
 
+    var mixer = mixitup('.products__inner-box');
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
 
-    
+      $('.product-slider__inner').slick({
+          dots: true,
+          arrows: false,
+          slidesToScroll: 4,
+          slidesToShow: 4
+      });
+
+
 });
