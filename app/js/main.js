3f4e35dc3b010f9fea855bad92256f8e64242c17
ehotzvezdolet <ehotzvezdolet@gmail.com>
2021-01-05 22:56:10 +0300

    added header adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    var mixer = mixitup('.products__inner-box');
+    
     $(".rate-star").rateYo({
         rating: 5,
         starWidth: "12px",
@@ -14,5 +14,12 @@ $(function(){
           slidesToShow: 4
       });
 
+      $('.menu__btn').on('click', function(){
+          $('.menu__list').slideToggle();
+      });
+      $('.header__btn-menu').on('click', function(){
+        $('.header__box').toggleClass('active');
+    });
 
+      var mixer = mixitup('.products__inner-box');
 });
